97f58f93dbb0f085006ae38f13884e3b0ce3e79b
Sabelo <35581721+SabeloVila@users.noreply.github.com>
2019-03-14 03:30:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+  $('#burger').on('click', function(){
+         $('#menubar').toggleClass('active');
+  });
+
+$('#close').on('click', function(){
+      $('#modeltab').fadeOut();
+});
+
+$('.openpage').on('click', function(){
+      $('#modeltab').fadeIn();
+});
+
+$('#viewpic').on('click', function(){
+       $('')
+})
